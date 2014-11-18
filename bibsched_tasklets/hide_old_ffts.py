418b751eedce503482705fc1f75c9969ed07cac1
Wojciech Ziółek <wojciech.ziolek@cern.ch>
2014-11-18 16:16:29 +0100

    HideOldFFTs: hide FFTs older than latest revision

---
+++ bibsched_tasklets/hide_old_ffts.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+##
+## This file is part of Invenio.
+## Copyright (C) 2014 CERN.
+##
+## Invenio is free software; you can redistribute it and/or
+## modify it under the terms of the GNU General Public License as
+## published by the Free Software Foundation; either version 2 of the
+## License, or (at your option) any later version.
+##
+## Invenio is distributed in the hope that it will be useful, but
+## WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+## General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with Invenio; if not, write to the Free Software Foundation, Inc.,
+## 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+
+"""
+Hide old FFTs.
+"""
+
+from invenio.bibdocfile import BibRecDocs, CFG_BIBDOCFILE_AVAILABLE_FLAGS
+from invenio.search_engine import perform_request_search
+
+
+def hide_old_ffts():
+    ids = perform_request_search(p="", of='intbitset')
+    for one_id in ids:
+        bibrec = BibRecDocs(one_id)
+        bibdoc = bibrec.list_bibdocs()[0]
+        latest_rev = bibdoc.get_latest_version()
+
+        i = 1
+        while i < latest_rev:
+            rev_file_types = []
+            for f in bibdoc.list_version_files(i):
+                if f.format not in rev_file_types:
+                    rev_file_types.append(f.format)
+            for file_type in rev_file_types:
+                bibdoc.set_flag(CFG_BIBDOCFILE_AVAILABLE_FLAGS[3], file_type, i)
+            i += 1
